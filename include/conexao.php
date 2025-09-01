d6dc2eb2b08b27b40b6bb79805013dea04529b53
João Vitor Silva Machado <86895133020@senacrs.edu.br>
2025-09-01 09:14:14 -0300

    main-1

---
+++ include/conexao.php
@@ -0,0 +1,8 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'empresa');
+
+$conexao = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);  
+?>
