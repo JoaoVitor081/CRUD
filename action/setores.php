908b6045184c9715235d421520fbcda1d143a095
Miguel Senger Naiba <04601730066@senacrs.edu.br>
2025-09-12 12:35:57 -0300

    commit

--- action/setores.php
+++ action/setores.php
@@ -5,11 +5,17 @@ include_once   '../include/conexao.php';
 
 // captura a acao dos dados
 $acao = $_GET['acao'];
-
+$id = $_GET['id'];
 // validacao
 switch ($acao) {
-    case 'value':
-        # code...
+    case 'excluir':
+        $sql = "DELETE FROM setores WHERE SetoresID = $id";
+        if (mysqli_query($conexao, $sql)) {
+            echo "Registro excluído com sucesso";
+        } else {
+            echo "Erro ao excluir registro: " . mysqli_error($conexao);
+        }
+        header("Location: ../lista-setores.php");
         break;
     
     default:
--- action/usuarios.php
+++ action/usuarios.php
@@ -5,11 +5,17 @@ include_once   '../include/conexao.php';
 
 // captura a acao dos dados
 $acao = $_GET['acao'];
-
+$id = $_GET['id'];
 // validacao
 switch ($acao) {
-    case 'value':
-        # code...
+    case 'excluir':
+        $sql = "DELETE FROM usuarios WHERE UsuarioID = $id";
+        if (mysqli_query($conexao, $sql)) {
+            echo "Registro excluído com sucesso";
+        } else {
+            echo "Erro ao excluir registro: " . mysqli_error($conexao);
+        }
+        header("Location: ../lista-usuarios.php");
         break;
     
     default:
